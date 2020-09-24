10291ed2b5ac6eae463c542527e55984af273b9b
Dandrezz <diegoaporterol@gmail.com>
2020-09-23 23:47:49 -0500

    build

---
+++ docs/precache-manifest.7ae4b5afe8283f0f46b5a55e75164642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dae4ab86c6106a9cf81940011f6792e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38d3d4dfe8a896a92ae9",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "31626f9898f677fcce94",
+    "url": "/static/css/main.a88a0523.chunk.css"
+  },
+  {
+    "revision": "38d3d4dfe8a896a92ae9",
+    "url": "/static/js/2.8be6c590.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8be6c590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31626f9898f677fcce94",
+    "url": "/static/js/main.36ea3602.chunk.js"
+  },
+  {
+    "revision": "34870de5b717690829ec",
+    "url": "/static/js/runtime-main.dc21d25e.js"
+  }
+]);
